460e05daa98305c5ffe5210380523c6d7257269d
yefrin327 <lazoyefrin904@gmail.com>
2024-12-19 19:44:35 -0600

    Add files via upload
    Agregado archivo Intex html

---
+++ script.js
@@ -0,0 +1,5 @@
+// Validación simple para el formulario de contacto
+document.querySelector("form").addEventListener("submit", function(e) {
+    alert("¡Gracias por contactarnos! Nos pondremos en contacto contigo pronto.");
+    e.preventDefault();
+});
